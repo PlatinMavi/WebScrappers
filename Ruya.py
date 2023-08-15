1a4ff88eed2b5aef8f67dd1f5176364681102610
Alper Gezer <a.yz07x@gmail.com>
2023-08-16 01:23:46 +0300

    Image Saving Function

--- Ruya.py
+++ Ruya.py
@@ -5,6 +5,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import datetime
+from PIL import Image
+import os
 
 class RuyaScrapper:
     def __init__(self) -> None:
@@ -118,8 +120,49 @@ class RuyaScrapper:
                     print("Hata",manga["name"])
         
         return returnies
+    
+    def GetMangaImage(self):
+        # data = self.GetAllMangas()
+        data = [{"name": "Reborn Ranker", "link":"https://www.ruyamanga.com/manga/reborn-ranker/"}]
+
+        options = ChromeOptions()
+        options.add_argument('--head')
+        options.add_argument("start-maximized")
+
+        output_directory = "Temp"  # Directory to save the images
+        os.makedirs(output_directory, exist_ok=True)
+
+        returnies = []
+        with Chrome(options=options) as driver:
+            for manga in data :
+                driver.get(manga["link"])
+                image_name = manga["link"].split("/")[-2]+"Ruya" + ".png"  # You can customize the image name here
+                image_path = os.path.join(output_directory, image_name)
+
+                WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CLASS_NAME, "summary_image")))
+                image_element = driver.find_element(By.CLASS_NAME, "summary_image").find_element(By.TAG_NAME, "img")
+
+                driver.execute_script("window.scrollTo(0,300)")
+                image_location = image_element.location
+                image_size = image_element.size
+                driver.save_screenshot(image_path)
+
+                viewport_height = driver.execute_script("return window.innerHeight;")
+                scroll_position = driver.execute_script("return window.pageYOffset;")
+
+                cropped_top = max(0, image_location['y'] - scroll_position)
+                cropped_bottom = min(viewport_height, image_location['y'] + image_size['height'] - scroll_position)
+
+                cropped_image = Image.open(image_path).crop((image_location['x'], cropped_top, 
+                                                            image_location['x'] + image_size['width'], 
+                                                            cropped_bottom))
+                cropped_image.save(image_path)
+                print(f"Image saved: {image_path}")
+
+        
 
     
 rs = RuyaScrapper()
-with open("log.txt", "w", errors="replace") as log_file:
-    print(rs.GetAllChapters(), file=log_file)
+# with open("log.txt", "w", errors="replace") as log_file:
+#     print(rs.GetAllChapters(), file=log_file)
+rs.GetMangaImage()
